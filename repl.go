c726c1f43a3c8b1a3a7b12cd5fc541948202864b
Shubham Bhatt <shubhambhatt92021@gmail.com>
2026-03-08 17:31:14 +0530

    implement REPL functionality and update .gitignore to include repl.log

--- repl.go
+++ repl.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
