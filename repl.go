435ed104116bcb584ba98712b8ebd2398b0d0d0b
Shubham Bhatt <shubhambhatt92021@gmail.com>
2026-03-09 02:56:23 +0530

    implement command handling in REPL with exit and help commands

--- repl.go
+++ repl.go
@@ -7,6 +7,27 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -17,8 +38,31 @@ func startRepl() {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", words[0])
+		command := getCommands()
+		cmd, exists := command[words[0]]
+
+		if exists {
+			cmd.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
 
 func cleanInput(text string) []string {
